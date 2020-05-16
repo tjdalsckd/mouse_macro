e941f45f248a05ac7b32c39f8271b9508e4246a7
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2020-05-16 16:12:00 +0900

    Update capture.py

--- capture.py
+++ capture.py
@@ -12,6 +12,7 @@ import mouse
 
 import numpy as np
 start = 0
+value = 99999
 print("Input checkvalue:")
 check_value =int(input())
 print("Input move1 x:")
@@ -23,11 +24,17 @@ move2_x =int(input())
 print("Input move2 y:")
 move2_y =int(input())
 
-
-
+allstop = 0
+ROI_SET = False
 def move():
     global value
+    global start
+
+    global allstop
     while 1:
+        if start == 1:
+           start = 2
+        if start>1:
            print(value)
            if value < check_value:
                print("move1")
@@ -41,12 +48,14 @@ def move():
                mouse.click()
                
            time.sleep(5)
+           if allstop ==1:
+                break;
  
 
 def set_roi():
     global ROI_SET, x1, y1, x2, y2
     global start
-    start = 1
+    
     ROI_SET = False
     print("Select your ROI using mouse drag.")
     while(mouse.is_pressed() == False):
@@ -62,46 +71,60 @@ def set_roi():
                 print("Your ROI : {0}, {1}, {2}, {3}".format(x1, y1, x2, y2))
 
                 ROI_SET = True
+                
                 return x1,y1,x2,y2
 
 
     
 def stop():
-   sys.exit(0)
+   global allstop
+   allstop = 1
  
-
-
-ROI_SET = False
-
-x1,y1,x2,y2 = set_roi()
-
-global value
-value =20
-th1 = threading.Thread(target=move)
-while True:
-    if start == 1:
-       th1.start()
-       start = 2
+def capture():
+   global x1
+   global x2
+   global y1
+   global y2
+   global allstop
+   global start
+   global value
+   global ROI_SET
+   x1,y1,x2,y2 = set_roi()
+   while True:
+    if allstop ==1:
+          break;
     if ROI_SET == True:
         image = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(x1, y1, x2, y2))), cv2.COLOR_BGR2RGB)
         cv2.imwrite('image.png',image)
+        start = 1
         try:
             value1=ocr_image('image.png',service='qq')
             value2 = re.findall("\d+",value1[0])
             value = float(value2[0])
         except:
-            value = 99999
+            value = 40
 
 
         cv2.imshow("image", image)
 
-        key = cv2.waitKey(100)
+        key = cv2.waitKey(10)
 
         if key == ord("q"):
 
             print("Quit")
-
+            allstop = 1
             break
- 
 
-cv2.destroyAllWindows()
+   cv2.destroyAllWindows()
+
+th1 = threading.Thread(target=move)
+th2 = threading.Thread(target=capture)
+th1.start()
+th2.start()
+
+while 1:
+    if allstop ==1:
+        break;
+    print("stopcommand : ",allstop,"\t read_value : ",value)
+    time.sleep(1)
+
